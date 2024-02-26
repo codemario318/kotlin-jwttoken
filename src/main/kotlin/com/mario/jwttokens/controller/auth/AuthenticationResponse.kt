6181baae68d570fbc803f1a427ae2999b4013670
mario <codemario318@gmail.com>
2024-02-26 14:34:41 +0900

    [ADD] jwt 토큰 인증 처리 추가

---
+++ src/main/kotlin/com/mario/jwttokens/controller/auth/AuthenticationResponse.kt
@@ -0,0 +1,5 @@
+package com.mario.jwttokens.controller.auth
+
+data class AuthenticationResponse(
+    val accessToken: String
+)
---
+++ src/main/kotlin/com/mario/jwttokens/service/AuthenticationService.kt
@@ -0,0 +1,36 @@
+package com.mario.jwttokens.service
+
+import com.mario.jwttokens.config.JwtProperties
+import com.mario.jwttokens.controller.auth.AuthenticationRequest
+import com.mario.jwttokens.controller.auth.AuthenticationResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class AuthenticationService(
+    private val authManager: AuthenticationManager,
+    private val userDetailsService: CustomUserDetailsService,
+    private val tokenService: TokenService,
+    private val jwtProperties: JwtProperties,
+) {
+    fun authentication(authRequest: AuthenticationRequest): AuthenticationResponse {
+        authManager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                authRequest.email,
+                authRequest.password
+            )
+        )
+
+        val user = userDetailsService.loadUserByUsername(authRequest.email)
+        val accessToken = tokenService.generate(
+            userDetails = user,
+            expirationDate = Date(System.currentTimeMillis() + jwtProperties.accessTokenExpiration)
+        )
+
+        return AuthenticationResponse(
+            accessToken = accessToken
+        )
+    }
+}
