d1a6b577946c29581d850d1154e4c80977677c44
mario <codemario318@gmail.com>
2024-02-26 10:23:35 +0900

    [INIT] Init kotlin + Spring boot + jwttoken

---
+++ src/main/kotlin/com/mario/jwttokens/JwttokensApplication.kt
@@ -0,0 +1,11 @@
+package com.mario.jwttokens
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwttokensApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwttokensApplication>(*args)
+}
