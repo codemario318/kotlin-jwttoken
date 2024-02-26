8e5019dc2d24a3d25dfbff7ecff8d7f4fb3b2346
mario <codemario318@gmail.com>
2024-02-26 12:03:51 +0900

    [ADD] 비밀번호 인코드 처리 추가

--- src/main/kotlin/com/mario/jwttokens/repository/UserRepository.kt
+++ src/main/kotlin/com/mario/jwttokens/repository/UserRepository.kt
@@ -2,34 +2,38 @@ package com.mario.jwttokens.repository
 
 import com.mario.jwttokens.model.Role
 import com.mario.jwttokens.model.User
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-class UserRepository {
+class UserRepository(
+    private val encoder: PasswordEncoder
+) {
     private val users = mutableListOf(
         User(
             id = UUID.randomUUID(),
             email = "user1@gmail.com",
-            password = "password",
+            password = encoder.encode("password"),
             role = Role.USER
         ),
         User(
             id = UUID.randomUUID(),
             email = "user2@gmail.com",
-            password = "password",
+            password = encoder.encode("password"),
             role = Role.ADMIN
         ),
         User(
             id = UUID.randomUUID(),
             email = "user3@gmail.com",
-            password = "password",
+            password = encoder.encode("password"),
             role = Role.USER
         )
     )
 
     fun save(user: User): Boolean {
-        return users.add(user)
+        val updated = user.copy(password = encoder.encode(user.password))
+        return users.add(updated)
     }
 
     fun findByEmail(email: String): User? {
